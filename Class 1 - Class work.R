09d009da8700acc771a685f71824a9bdad0493cf
karimillab <166187422+karimillab@users.noreply.github.com>
2024-04-13 23:19:25 -0400

    Update Class 1 - Class work.R

--- Class 1 - Class work.R
+++ Class 1 - Class work.R
@@ -1,9 +1,12 @@
+# Variables
 v1 <- 4.5
 v2 <- 2
 v3 <- v1/v2
 v3
 v4 <- sqrt(v1)
 v4
+
+# More Variables
 greet <- "Hello,"
 name <-" Bob"
 message <- paste(greet, name)
@@ -23,6 +26,16 @@ typeof(y)
 z <- 2+3i
 typeof(z)
 
+# #character
+a <- "h"
+typeof(a)
+
+#logical
+q <- T
+typeof(q)
+p <- FALSE
+typeof(p)
+
 # Basic Math
 a <- 5
 b <- 3
@@ -31,6 +44,7 @@ d <- a - b
 e <- a * b
 f <- a / b
 
+#Basic logics and complex operations
 4 <5
 10>100
 4==5
@@ -38,12 +52,13 @@ f <- a / b
 result <- 4 < 5
 result
 typeof(result)
-result <- 4 < 5
-result
-typeof(result)
+
 result2 <- !(5>1)
 result2
+
 result | result2
 result & result2
+
 isTRUE(result)
+
 #Bharath
